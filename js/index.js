5e9ae111030b0176341b93751d67e534216cd1d6
Lukas Kveraga <77985925+lukepas@users.noreply.github.com>
2022-01-17 12:53:35 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
+const nav = document.querySelector('.nav');
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
+    nav.style.display = !nav.style.display ? "block" : "";
     document.body.classList.toggle('nav-open');
 });
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
+        nav.style.display = !nav.style.display ? "block" : "";
         document.body.classList.remove('nav-open');
     })
 })
